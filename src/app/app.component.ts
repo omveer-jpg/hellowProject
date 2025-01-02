bf30d80234ebafbf75b599f6af95069e55002c17
omveer singh <omveer10101997rawat@gmail.com>
2025-01-02 21:35:44 +0530

    staged changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnChanges, SimpleChanges,OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnChanges,OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   title = 'hellow_project';
 }
